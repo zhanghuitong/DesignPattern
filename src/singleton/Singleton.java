79d20890748c6a704325b9a5061b62f6456a7702
zhanghuitong <zhanghui_tong@163.com>
2018-12-25 22:06:06 +0800

    singleton

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class Singleton {
+	//利用一个静态变量来记录Singleton类的唯一实例
+	private static Singleton uniqueInstance;
+	
+	private Singleton() {} //把构造器设为私有 ,只有Singleton类内才能调用构造器
+	
+	public static Singleton getInstance() {
+		if(uniqueInstance == null) {
+			uniqueInstance=new Singleton();
+		}
+		return uniqueInstance;
+	}
+}
