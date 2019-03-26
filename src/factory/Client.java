3cd8b353a10ce3265b493be83251caaaff2db0ba
zhanghuitong <zhanghui_tong@163.com>
2019-03-26 08:26:27 +0800

    移除不必要文件,增加md文档

--- src/factory/Client.java
+++ src/factory/Client.java
@@ -1,5 +1,8 @@
 package factory;
 
 public class Client {
-
+	public static void main(String[] args) {
+		AbstractFacatory abstractFacatory = new CheesePizzaFactory();
+		Pizza pizza = abstractFacatory.createPizza();
+	}
 }
